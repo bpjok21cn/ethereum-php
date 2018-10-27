2ada2342c7797a53bd693f1ea6353fb5dcde4602
EasonZhong <164823178@qq.com>
2018-10-27 23:21:35 +0800

    交易hash接收者状态类型更新

--- src/EthereumPHP/Types/TransactionReceipt.php
+++ src/EthereumPHP/Types/TransactionReceipt.php
@@ -4,8 +4,42 @@ namespace EthereumPHP\Types;
 
 class TransactionReceipt
 {
-    // TODO: do it
-    public function __construct($result)
+    private $blockHash;
+    private $blockNumber;
+    private $contractAddress;
+    private $cumulativeGasUsed;
+    private $from;
+    private $gasUsed;
+    private $logs;
+    private $logsBloom;
+    private $status;
+    private $to;
+    private $transactionHash;
+    private $transactionIndex;
+    
+    public function __construct(array $response)
     {
+        $this->blockHash = new BlockHash($response['blockHash']);
+        $this->blockNumber = hexdec($response['blockNumber']);
+        if ($response['contractAddress']) {
+            $this->contractAddress = new Address($response['contractAddress']);
+        }
+        $this->cumulativeGasUsed = hexdec($response['cumulativeGasUsed']);
+        $this->from = new Address($response['from']);
+        $this->gasUsed = hexdec($response['gasUsed']);
+        $this->logs = $response['logs'];
+        $this->logsBloom = hexdec($response['logsBloom']);
+        $this->status = hexdec($response['status']);
+        if ($response['to']) {
+            $this->to = new Address($response['to']);
+        }
+        $this->transactionHash = new TransactionHash($response['transactionHash']);
+        $this->transactionIndex = hexdec($response['transactionIndex']);
     }
+
+    public function status(): int
+    {
+        return $this->status;
+    }
+
 }
